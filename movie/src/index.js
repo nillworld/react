972e95f1f0f896e3d0f0be0303620fb8b72561b2
nill <wwaosldk@gmail.com>
2021-11-07 17:41:49 +0900

    study,5

--- movie/src/index.js
+++ movie/src/index.js
@@ -1,13 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
+// import App from "./App";
+import AppTest from "./AppTest";
 //import StateTest from './StateTest';
 import { BrowserRouter } from "react-router-dom";
 ReactDOM.render(
   <React.StrictMode>
     <BrowserRouter>
       {/* <StateTest/> */}
-      <App />
+      <AppTest />
     </BrowserRouter>
   </React.StrictMode>,
   document.getElementById("root")
