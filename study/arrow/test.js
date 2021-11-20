0e09e516f88c29c245177fe2d46518c1b574d5e2
nill <wwaosldk@gmail.com>
2021-11-20 22:59:40 +0900

    react arrow test

--- study/arrow/test.js
+++ study/arrow/test.js
@@ -1,22 +1,31 @@
-const test1 = () => "yeah~~!!";
+console.log(test4);
+console.log(test1);
+
+const test = (test) => test * 3;
+const test_2 = (test) => {
+  console.log(test * 3);
+};
+
+var test1 = () => "test1";
 
 const test2 = () => {
-  "yeah~~!!";
+  "test2";
 };
 
 const test3 = () => {
-  return "yeah~~!!";
+  return "test3";
 };
 
 function test4() {
-  return "yeah!";
+  return "test4";
 }
 
 function test5() {
-  return "yeah!";
+  return "test5";
 }
 
 (() => {
   console.log("yeah");
 })();
+console.log(test_2(10));
 console.log(test4());
