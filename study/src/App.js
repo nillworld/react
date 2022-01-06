c96dae2399047f79a7c3cebb5ad5f7842f74ed42
nill <wwaosldk@gmail.com>
2022-01-06 23:27:58 +0900

    axios...

--- study/src/App.js
+++ study/src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 // https://sangminem.tistory.com/28  - js 크롤링
@@ -6,7 +7,32 @@ const cheerio = require("cheerio");
 const fs = require("fs");
 
 function App() {
-  return <div className="App">hi</div>;
+  const [resultList, setResultList] = useState([]);
+  const [cnt, setCnt] = useState(0);
+
+  const getHTML = (url) => {
+    return new Promise((resolve) => {
+      delay(300).then(() => {
+        axios.get(url).then((data) => {
+          resolve(data);
+        });
+      });
+    });
+  };
+
+  const getHTML2 = async (url) => {
+    await delay(300);
+    return await axios.get(url);
+  };
+
+  const delay = (ms) => setTimeout(ms);
+
+  return (
+    <div className="App">
+      <h1>hi</h1>
+      <div>{getHTML2(`https://sangminem.tistory.com/28`)}</div>
+    </div>
+  );
 }
 
 export default App;
